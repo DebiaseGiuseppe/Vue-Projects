477fcb30dec1dabd2099fa4eeea881ad0574812d
Giuseppe De Biase <giuseppe.debiase@syntlogo.de>
2024-06-10 16:27:17 +0200

    projects

---
+++ vue-first-app/src/App.vue
@@ -0,0 +1,77 @@
+<template>
+  <h1> {{ title }}</h1>
+  <p>Welcome...</p>
+  <div v-if="showModel">
+    <MyModel :theme="theme" @close="toggleModal">
+      <template v-slot:links>
+        <a href="#"> Sign up now</a>
+        <a href="#"> more info</a>
+      </template>
+      <h1>Ninja</h1>
+    </MyModel>
+  </div>
+
+  <div v-if="showModel2">
+    <MyModel :theme="theme" @close="toggleModal2">
+      <template v-slot:links>
+        <a href="#"> Sign up now 2</a>
+        <a href="#"> more info 2</a>        
+      </template>
+      <h1>Second module</h1>
+    </MyModel>
+
+  </div>
+  
+
+
+  
+  <button @click="toggleModal">Open modal</button> <br>
+  <button @click="toggleModal2">Open modal 2</button>
+</template>
+
+<script>
+
+import MyModel from './components/MyModel.vue'
+
+export default {
+  name:"App",
+  components: {MyModel},
+  data(){
+    return {
+      title:"my first new app",
+      header: "Sing up for the Giveaway",
+      text: "nice text",
+      theme: "sale",
+      showModel: false,
+      showModel2: false
+    }
+  },
+  methods: {
+    toggleModal(){
+      this.showModel = !this.showModel
+    },
+    toggleModal2(){
+      this.showModel2 = !this.showModel2
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+
+h1 {
+  border-bottom: 1px solid #ddd;
+  display: inline-block;
+  padding-bottom: 10px;
+
+}
+</style>
---
+++ vue-first-app/src/components/MyModel.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="backdrop" @click.self="closeModule">
+        <div class="model" :class="{ sale: theme === 'sale'}">
+            <slot></slot>
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+
+<script>
+export default{
+    props: ['theme'],
+    methods: {
+        closeModule(){
+            this.$emit('close')
+        }
+    }
+}
+
+</script>
+
+
+<style>
+.model{
+    width: 400px;
+    padding: 20px;
+    margin: 100px auto;
+    background: white;
+    border-radius: 10px;
+}
+
+.backdrop{
+    top: 0;
+    position: fixed;
+    background: rgba(0,0,0,0.5);
+    width: 100%;
+    height: 100%;
+}
+
+.model p{
+    font-style: normal;
+}
+
+
+.model.sale {
+    background: red;
+    color: white;
+}
+.model.sale h1 {
+    color: white;
+}
+
+</style>
